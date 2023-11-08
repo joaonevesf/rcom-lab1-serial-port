119d8c232b65bb7ff7c930ce9a5a174f3517f1bf
João Fernandes <93282636+joaonevesf@users.noreply.github.com>
2023-11-08 10:09:47 +0000

    Update application_layer.c

--- code/src/application_layer.c
+++ code/src/application_layer.c
@@ -292,7 +292,7 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     }
 
     printf("Time elapsed: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
-    double bitRate = (double) globalFileSize / (double)(end - start) * CLOCKS_PER_SEC;
+    double bitRate = (double) globalFileSize * 8 / (double)(end - start) * CLOCKS_PER_SEC;
     printf("Bitrate: %f\n", bitRate);
 
     return;
